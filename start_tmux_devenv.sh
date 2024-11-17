2c912350b6fde438c0109e1a4d32d3a06d906a33
Quakkels <1100670+quakkels@users.noreply.github.com>
2024-11-17 12:38:27 -0600

    fix tmux script

--- start_tmux_devenv.sh
+++ start_tmux_devenv.sh
@@ -4,8 +4,9 @@ SESSION_NAME="StatsForager"
 
 if ! tmux has-session -t $SESSION_NAME 2>/dev/null; then
 
-	# database window
 	tmux new-session -d -s $SESSION_NAME -n "statsforagerdata"
+
+	# database window
 	tmux split-window -h -t $SESSION_NAME:0
 	tmux send-keys -t $SESSION_NAME:0.0 'cd ./statsforagerdata' C-m
 	tmux send-keys -t $SESSION_NAME:0.0 './dev_runDocker.sh' C-m
@@ -15,15 +16,16 @@ if ! tmux has-session -t $SESSION_NAME 2>/dev/null; then
 
 	# development window
 	tmux new-window -t $SESSION_NAME -n "statsforagerweb"
-	tmux send-keys -t $SESSION_NAME:1 'cd ./statsforagerweb' C-m
-	tmux send-keys -t $SESSION_NAME:1 'nvim .' C-m
-	tmux split-window -h -t $SESSION_NAME:2
+	tmux split-window -h -t $SESSION_NAME:1
+	tmux send-keys -t $SESSION_NAME:1.0 'cd ./statsforagerweb' C-m
+	tmux send-keys -t $SESSION_NAME:1.0 'nvim .' C-m
+	tmux send-keys -t $SESSION_NAME:1.1 'cd ./statsforagerweb' C-m
 	tmux send-keys -t $SESSION_NAME:1.1 'make run-native' C-m
 
 	# client window
 	tmux new-window -t $SESSION_NAME -n "statsforagerclient"
-	tmux send-keys -t $SESSION_NAME:2 -n 'cd ./statsforagerclient' C-m
-	tmux send-keys -t $SESSION_NAME:2 -n 'npm run dev' C-m
+	tmux send-keys -t $SESSION_NAME:2.0 'cd ./statsforagerclient' C-m
+	tmux send-keys -t $SESSION_NAME:2.0 'npm run dev' C-m
 fi
 
 # Attach to the session and select the 'database' window
